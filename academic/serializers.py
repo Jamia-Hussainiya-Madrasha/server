bd90ac6ee5e4dd4d4e7225914fd46aeacebdbb76
shakibosd <syednazmusshakib94@gmail.com>
2025-02-20 23:24:40 +0600

    fet(academic) : academic

--- academic/serializers.py
+++ academic/serializers.py
@@ -4,4 +4,4 @@ from .models import Academic
 class AcademicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Academic
-        fields = ['id', 'class_name', 'class_title', 'class_description', 'student_count', 'class_created', 'class_update']
+        fields = ['id', 'class_name', 'class_title', 'class_description', 'student_count', 'number_seat', 'class_created', 'class_update']
--- server/settings.py
+++ server/settings.py
@@ -27,13 +27,14 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic', 
     #pre define
     'django.contrib.admin',
     'django.contrib.auth',
@@ -55,6 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",  
     "django.middleware.common.CommonMiddleware",
@@ -148,6 +150,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = "/media/"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
