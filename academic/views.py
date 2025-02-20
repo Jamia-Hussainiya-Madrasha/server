d16ec26b56e62f3ae2bb643e4b5765d973bc204a
shakibosd <syednazmusshakib94@gmail.com>
2025-02-21 05:16:08 +0600

    fet(academic, admission: models update

--- academic/views.py
+++ academic/views.py
@@ -7,6 +7,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from  rest_framework import status
 from rest_framework.exceptions import NotFound
+from rest_framework.filters import OrderingFilter, SearchFilter
 
 class AcademicPagination(PageNumberPagination):
     page_size = 5 # প্রতিটি পেজের মধ্যে ৫ টি করে আইটেম দেখাবে।
@@ -17,18 +18,27 @@ class AcademicListApiView(generics.ListAPIView):
     queryset = Academic.objects.all().order_by('id')
     serializer_class = AcademicSerializer
     pagination_class = AcademicPagination
-    filter_backends = [DjangoFilterBackend, OrderingFilter]
-    filterset_fields = {
-        'class_name': ['exact', 'icontains'],
-    }
-    ordering_fields = ['class_title', 'class_name']
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    filterset_fields = ['class_name']
+    ordering_fields = ['class_title']
+    search_fields = ['class_name', 'class_title']
 
-    def list(self, request, *args, **kwargs):
-        response = super().list(request, *args, **kwargs)
+    def list(self, request,  *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response({
+                "success": True,
+                "message": "Teachers fetched successfully",
+                "data": serializer.data
+            })
+        serializer = self.get_serializer(queryset, many=True)
         return Response({
-            "status": "success",
-            "total_classes": len(response.data["results"]),
-            "data": response.data
+            "success": True,
+            "message": "Teachers fetched successfully",
+            "data": serializer.data
         })
 
 class AcademicDetailsListApiView(generics.RetrieveAPIView):
--- admission/views.py
+++ admission/views.py
@@ -2,10 +2,17 @@ from rest_framework import generics, filters
 from .models import Admission
 from .serializers import AdmissionSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination
+
+class AdmissionPagination(PageNumberPagination):
+    page_size = 5 # প্রতিটি পেজের মধ্যে ৫ টি করে আইটেম দেখাবে।
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class AdmissionListCreateView(generics.ListCreateAPIView):
     queryset = Admission.objects.all().order_by('-admission_created')
     serializer_class = AdmissionSerializer
+    pagination_class = AdmissionPagination
 
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
     filterset_fields = ['class_level', 'seat_availability']
