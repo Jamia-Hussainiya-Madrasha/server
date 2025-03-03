8c120d46d541e5b03ff04c7a862a72f3ea4abd67
shakibosd <syednazmusshakib94@gmail.com>
2025-03-03 22:41:07 +0600

    feat(image): image model change added by verbose_name_plural

--- image/models.py
+++ image/models.py
@@ -3,3 +3,9 @@ from django.db import models
 # Create your models here.
 class Images(models.Model):
     img = models.CharField(default='', max_length=1000)
+
+    def __str__(self):
+        return f'{self.img}'
+    
+    class Meta:
+        verbose_name_plural = 'ইমেজ বা ছবি'
